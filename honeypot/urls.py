dda8756589858046c0194b2eaefe2accbb79feda
srvshbskr <srvshbskr@gmail.com>
2024-04-23 21:23:06 +0530

    initial commit

---
+++ honeypot/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('notrealadmin/', admin.site.urls),
+]
